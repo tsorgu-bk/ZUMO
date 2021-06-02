a3badd0ce6d07f5505287844d6dd9738cd95f36c
KNikodem <nikodemklimala@interia.eu>
2021-06-03 00:55:35 +0200

    ICM sensor init + lib for BHI160

--- targets/arm/Core/Src/main.cpp
+++ targets/arm/Core/Src/main.cpp
@@ -6,9 +6,9 @@
 //#include "UART_STM32F4x.h"
 #include "I2C_STM32F4x.h"
 #include "BME280.h"
+#include "bhy.h"
 #include "stdio.h"
-
-
+#include <ICM20948.h>
 
 GPIO_STM32F4x LED1(LED1_GPIO_Port,LED1_Pin);
 GPIO_STM32F4x LED2(LED2_GPIO_Port,LED2_Pin);
@@ -18,13 +18,21 @@ extern I2C_HandleTypeDef hi2c1;
 extern I2C_HandleTypeDef hi2c2;
 I2C_STM32F4x i2c(hi2c1);
 BME280 bme280( i2c, 0b1110110);
-
+BHYSensor bhi160(i2c);
+ICM20948 myIMU(i2c);
 extern UART_HandleTypeDef huart1;
 
 uint8_t data_buffer[40]= {0};
 
 //UART_STM32F4x usart;
 extern "C"
+//
+
+
+// test
+
+
+// test
 int Main(){
     // usart.init();
     if(bme280.init()){
@@ -35,14 +43,45 @@ int Main(){
         HAL_UART_Transmit(&huart1, data_buffer, length,10);
     }
     bme280.set_enable(true);
+    // icm20948
+    // Reset ICM20948
+    myIMU.writeByte(ICM20948_ADDRESS, PWR_MGMT_1, READ_FLAG);
+    HAL_Delay(100);
+    myIMU.writeByte(ICM20948_ADDRESS, PWR_MGMT_1, 0x01);
+
+
+    // Read the WHO_AM_I register, this is a good test of communication
+    char c = myIMU.readByte(ICM20948_ADDRESS, WHO_AM_I_ICM20948);
+    // Start by performing self test and reporting values
+    myIMU.ICM20948SelfTest(myIMU.selfTest);
+    myIMU.calibrateICM20948(myIMU.gyroBiasDef, myIMU.accelBiasDef);
+    // end icm20948
     while(true){
         LED1.toggle();
         HAL_Delay(100);
         LED2.toggle();
         HAL_Delay(100);
         bme280.run_measurements();
-        auto length = sprintf(reinterpret_cast<char *>(data_buffer), "Temperature: %f , Pressure: %lu \n ", bme280.get_last_temperature(), bme280.get_last_pressure());
+        auto length = sprintf(reinterpret_cast<char *>(data_buffer), "Temperature: %.2f , Pressure: %lu \n ", bme280.get_last_temperature(), bme280.get_last_pressure());
         HAL_UART_Transmit(&huart1, data_buffer, length,10);
-     }
+        length = sprintf(reinterpret_cast<char *>(data_buffer), "kappa: %c \n ", c);
+        HAL_UART_Transmit(&huart1, data_buffer, length,10);
+
+        /*if (bhi160.status == BHY_OK)
+            return true;
+
+        if (bhi160.status < BHY_OK) All error codes are negative
+        {
+            Serial.println("Error code: (" + String(bhi160.status) + "). " + bhi160.getErrorString(bhi160.status));
+
+            return false;  Something has gone wrong
+        }
+        else All warning codes are positive
+        {
+            Serial.println("Warning code: (" + String(bhi160.status) + ").");
+
+            return true;
+        }*/
+    }
     return 0;
 }
